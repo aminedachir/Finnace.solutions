8ec8b7e0818c375279645ad6112c346fe76bb4ab
aminedachir <dachiramine110@gmail.com>
2021-05-24 16:18:13 +0000

    add cash

--- application.py
+++ application.py
@@ -38,7 +38,6 @@ db = SQL("sqlite:///finance.db")
 if not os.environ.get("API_KEY"):
     raise RuntimeError("API_KEY not set")
 
-
 @app.route("/")
 @login_required
 def index():
@@ -46,23 +45,17 @@ def index():
     username = db.execute("SELECT username FROM users WHERE id = ?", session["user_id"])[0]["username"]
     return render_template("index.html",amount=usd(amount),username=username )
 
-
 @app.route("/buy", methods=["GET", "POST"])
 @login_required
 def buy():
     if request.method == "POST":
-
         stock = lookup(request.form.get("symbol"))
-
         if stock == None:
             return "<script>alert('Invalid Symbol')</script>"
-
         shares = int(request.form.get("shares"))
-
         cash = db.execute("SELECT cash FROM users WHERE id = ?", session["user_id"])[0]["cash"]
         if amount > cash:
             return "<script>alert('Can't Afford')</script>"
-
     else:
         return render_template("buy.html")
 
@@ -76,62 +69,41 @@ def history():
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-
     session.clear()
-
     if request.method == "POST":
-
         username = request.form.get("username")
         password1 = request.form.get("password")
         password2 = request.form.get("confirm")
-
         rows = db.execute("SELECT * FROM users WHERE username = :username",
                           username=username)
-
         if len(rows) != 0:
             return "<script>alert('username exists')</script>"
-
         elif username == password1:
             return "<script>alert('don't write your username in password)</script>"
-
         elif len(password1) < 8:
             return "<script>alert('password should take 8 letter a min')</script>"
-
         elif password1 != password2:
             return "<script>alert('password and confirm password not match')</script>"
-
         else:
             password = generate_password_hash(password1)
-
             db.execute("INSERT INTO users (username, hash) VALUES (:username, :password)", username=username, password=password)
-
             session["user_id"] = db.execute("SELECT id FROM users WHERE username = :username", username=username)[0]["id"]
-
             return redirect("/")
-
     else:
         return render_template("register.html")
 
-
 @app.route("/login", methods=["GET", "POST"])
 def login():
-
     session.clear()
-
     if request.method == "POST":
-
         rows = db.execute("SELECT * FROM users WHERE username = :username",
                           username=request.form.get("username"))
-
         if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
             return apology("invalid username and/or password Try again!", 403)
-
         session["user_id"] = rows[0]["id"]
-
         return redirect("/")
     else:
         return render_template("login.html")
-
 
 @app.route("/logout")
 def logout():
@@ -143,19 +115,14 @@ def logout():
     # Redirect user to login form
     return redirect("/")
 
-
 @app.route("/quote", methods=["GET", "POST"])
 @login_required
 def quote():
     if request.method == "POST":
-
         stock = lookup(request.form.get("symbol"))
-
         if stock == None:
             return "<script>alert('Invalid symbol')</script>"
-
         return render_template("quoted.html", stock=stock)
-
     else:
         return render_template("quote.html")
 
@@ -164,17 +131,13 @@ def quote():
 def sell():
     if request.method == 'POST':
         shares = int(request.form.get("shares"))
-
         if shares == 0:
             return "<script>alert('enter a number of shares')</script>"
-
         stock = lookup(request.form.get("symbol"))
-
         stocks = db.execute("SELECT no_shares FROM stocks WHERE user_id = ? AND stock_symbol = ?",
                                  session["user_id"], stock["symbol"])
         sharess = stocks[0]["not shares"]
         if shares > sharess:
-
             return "<script>alert('You do not have this number of shares')</script>"
         return render_template("sell.html", stocks=stocks)
     else:
@@ -208,7 +171,6 @@ def cash():
         return redirect("/")
     else:
         return render_template("cash.html")
-
 
 def errorhandler(e):
     """Handle error"""
